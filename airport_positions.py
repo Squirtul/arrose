02779caf82ecc8a9c5a5a6e13930b54c0c1346a5
Squirtul <pineapplecrazyav@gmail.com>
2026-09-16 19:23:53 +0100

    Update airport_positions.py

--- airport_positions.py
+++ airport_positions.py
@@ -118,7 +118,7 @@ AIRPORT_POSITIONS = {
     "EDDM": {
         "DEL": ["EDDM_DEL"],
         "GND": ["EDDM_N_GND", "EDDM_S_GND", "EDDM_1_GND", "EDDM_2_GND"],
-        "TWR": ["EDDM_N_TWR"],
+        "TWR": ["EDDM_N_TWR", "EDDM_S_TWR"],
         "APP": ["EDDM_ND_APP", "EDDM_NL_APP", "EDDM_NH_APP", "EDDM_NX_APP", "EDDM_SD_APP", "EDDM_SH_APP"],
         "ACC": ["EDMM_ALB_CTR", "EDMM_STA_CTR", "EDMM_ZUG_CTR"],
     },
